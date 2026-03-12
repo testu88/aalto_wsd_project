de3fd2eefbd8a9c5cfb18400fdc3d98d18258209
testu88 <testu88@users.noreply.github.com>
2026-03-12 16:39:25 +0800

    Initial commit

---
+++ walking-skeleton/server/app.js
@@ -0,0 +1,25 @@
+import { Hono } from "@hono/hono";
+import { cors } from "@hono/hono/cors";
+import { logger } from "@hono/hono/logger";
+import postgres from "postgres";
+
+const app = new Hono();
+
+// create instance of database client
+const sql = postgres();
+
+app.use("/*", cors());
+app.use("/*", logger());
+
+let visits = 0;
+app.get("/api/visits", (c) => {
+    visits++;
+    return c.json({visits});
+});
+
+// retrieve todos from database on requests to /api/todos
+app.get("/api/todos", async (c) => {
+    const todos = await sql`SELECT * FROM todos`;
+    return c.json(todos);
+})
+export default app;
