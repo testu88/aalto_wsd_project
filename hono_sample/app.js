c413ad3a3956707fb4e9fc85cb9adc32180cf5e6
testu88 <testu88@users.noreply.github.com>
2026-03-20 18:45:40 +0800

    Edit aap.js

--- hono_sample/app.js
+++ hono_sample/app.js
@@ -4,30 +4,33 @@ const app = new Hono();
 
 app.get("/", (c) => {
     let result = "";
-    let number1 = 0;
-    let number2 = 0;
-    if (c.req.query("operation") && c.req.query("number1") && c.req.query("number2")){
-        number1 = Number(c.req.query("number1"));
-        number2 = Number(c.req.query("number2"));
-        if (c.req.query("operation") === "sum"){
-            result = number1+ number2;
-        } else if (c.req.query("operation") === "difference"){
-            result = Number(number1) - Number(number2);
-        } else if (c.req.query("operation" !== "sum") || c.req.query("operation") !== "difference"){
+    let number1 = Number(c.req.query("number1"));
+    let number2 = Number(c.req.query("number2"));
+    let operation = c.req.query("operation");
+    if (operation){
+        if (operation === "sum"){
+            if (number1 && number2){
+               
+            result = number1 + number2;
+            } else if (number1){
+                result = number1 + 0;
+            } else if (number2){
+                result = 0 + number2;
+            }
+        } else if (operation === "difference"){
+            if (number1 && number2){
+            result = number1 - number2;
+            } else if (number1){
+                result = number1 - 0;
+            } else if (number2){
+                result = 0 - number2;
+            }
+        } else {
             result = "Invalid operation.";
         };
-    } else if (!c.req.query("operation")){
-        result = "Invalid operation.";
-    }else if (c.req.query("number1")){
-        number1 = Number(c.req.query("number1"));
-        result = number1+number2;
-    } else if (c.req.query("number2")){
-        number2 = Number(c.req.query("number2"));
-        result = number1+number2;
     } else {
         result = "Invalid operation.";
-    };
-
+    } 
     return c.text(result);
 });
 
